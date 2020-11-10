a072ccb8be283329554425dd1f0500892f2288cb
Shinhakcheol <73805118+Shinhakcheol@users.noreply.github.com>
2020-11-10 17:05:57 +0900

    1109

---
+++ Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+	
+	String name;
+	int age;
+	double height;
+	int weight;
+	int subject[];
+	Person man;
+	
+	public static void main(String[] arg) {
+		int n=0;
+		
+		System.out.println(n);
+	}
+	
+
+}
